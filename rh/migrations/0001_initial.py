fd781607fc5e8254a8ee9898b985bdbf7609f237
Oliver Wilkerson <oliver.wilkerson@gmail.com>
2014-03-15 06:04:23 -0500

    finally checking in the initial version of this.

---
+++ rh/migrations/0001_initial.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Heroine'
+        db.create_table('rh_heroine', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=140)),
+            ('nickname', self.gf('django.db.models.fields.CharField')(max_length=140)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=150)),
+            ('birthdate', self.gf('django.db.models.fields.DateField')()),
+            ('deathdate', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('country', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('is_public', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('hero_image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('description_html', self.gf('django.db.models.fields.TextField')()),
+            ('description_text', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('rh', ['Heroine'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Heroine'
+        db.delete_table('rh_heroine')
+
+
+    models = {
+        'rh.heroine': {
+            'Meta': {'ordering': "['birthdate', 'name']", 'object_name': 'Heroine'},
+            'birthdate': ('django.db.models.fields.DateField', [], {}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'deathdate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'description_html': ('django.db.models.fields.TextField', [], {}),
+            'description_text': ('django.db.models.fields.TextField', [], {}),
+            'hero_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'nickname': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '150'})
+        }
+    }
+
+    complete_apps = ['rh']
---
+++ rh/models.py
@@ -0,0 +1,110 @@
+import markdown
+import json
+
+from django.conf import settings
+from django.db import models
+from django.utils.html import strip_tags
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+# la la la ladeez
+
+def unescape(text):
+    """
+    Do reverse escaping.
+    """
+    return text.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"').replace('&#39;', '\'')
+
+class Heroine(models.Model):
+  """All the data for a heroine goes here."""
+  name = models.CharField(max_length=140)
+  nickname = models.CharField(max_length=140)
+  slug = models.SlugField(max_length=150)
+  birthdate = models.DateField()
+  # what if they're not dead?
+  deathdate = models.DateField(null=True,blank=True)
+  country = models.CharField(max_length=100)
+  is_public = models.BooleanField(default=False)
+
+  # TODO: break this into layers
+  hero_image = models.ImageField(upload_to='heroines/portraits',help_text='The image in portrait mode.')
+
+  # TODO: make these composite from the layers
+  grid_image_thumbnail = ImageSpecField(source='hero_image',
+                                      processors=[ResizeToFill(150, 150)],
+                                      format='PNG',
+                                      options={'quality': 100})
+
+  timeline_image_thumbnail = ImageSpecField(source='hero_image',
+                                      processors=[ResizeToFill(75, 75)],
+                                      format='PNG',
+                                      options={'quality': 100})
+
+  description = models.TextField(
+    help_text='Uses <a href="http://daringfireball.net/projects/markdown/" '
+               'target="_blank">Markdown</a>')
+  description_html = models.TextField('HTML version')
+  description_text = models.TextField('Plain text version')
+
+  def __str__(self):
+    return self.name
+
+  def save(self, *args, **kwargs):
+    self.description_html = markdown.markdown(self.description, extensions=['extra', 'admonition', 'headerid', 'sane_lists'])
+    # Remove tags which was generated with the markup processor
+    text = strip_tags(self.description_html)
+    # Unescape entities which was generated with the markup processor
+    self.description_text = unescape(text)
+
+    super(Heroine, self).save(*args, **kwargs)
+
+    heroines = Heroine.objects.all()
+    public_heroines = []
+    all_heroines = []
+
+    for h in heroines:
+      birthdate_string = None
+      if h.birthdate:
+        birthdate_string = h.birthdate.strftime('%Y-%m-%d')
+      deathdate_string = None
+      if h.deathdate:
+        deathdate_string = h.birthdate.strftime('%Y-%m-%d')
+
+      heroine_object = {
+        'name': h.name,
+        'nickname': h.nickname,
+        'slug': h.slug,
+        'birthdate': birthdate_string,
+        'deathdate': deathdate_string,
+        'country': h.country,
+        'hero_image': h.hero_image.url,
+        'grid_image_thumbnail': h.grid_image_thumbnail.url,
+        'timeline_image_thumbnail': h.timeline_image_thumbnail.url,
+      }
+
+      if h.is_public:
+        public_heroines.append(heroine_object)
+      all_heroines.append(heroine_object)
+
+    public_json_filename = '{0}/heroines.json'.format(settings.MEDIA_ROOT)
+    private_json_filename = '{0}/heroines-preview.json'.format(settings.MEDIA_ROOT)
+    try:
+      fh = open(public_json_filename, 'w')
+      public_heroines_json = json.dump(public_heroines, fh)
+      fh.close()
+    except IOError:
+      print('Failed to write json updates for heroines at {0}'.format(public_json_filename))
+
+    try:
+      fh = open(public_json_filename, 'w')
+      private_json_filename = json.dump(all_heroines, fh)
+      fh.close()
+    except IOError:
+      print('Failed to write json updates for heroines at {0}'.format(private_json_filename))
+
+
+  class Meta:
+    ordering = ['birthdate','name']
+    app_label = 'real_heroines'
+    db_table = 'rh_heroine'
+  
