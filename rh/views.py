d35c54477d62d7b2852e75670bdd60f33628f30f
Oliver Wilkerson <oliver.wilkerson@gmail.com>
2014-03-20 01:35:39 -0500

    portraits working; no transitions;

--- rh/views.py
+++ rh/views.py
@@ -11,20 +11,43 @@ from rh.models import Heroine
 def home(request, slug=None):
   context = {}
   template = loader.get_template('index.jade')
+  selected_heroine = None
+  previous_heroine = None
+  next_heroine = None
 
   if request.user.is_staff:
     context['heroines'] = Heroine.objects.all()
     if slug is not None:
       try:
-        context['selected_heroine'] = Heroine.objects.get(slug=slug)
+        selected_heroine = Heroine.objects.get(slug=slug)
+        try:
+          previous_heroine = Heroine.objects.filter(birthdate__lt=selected_heroine.birthdate).order_by('-birthdate')[0]
+        except IndexError:
+          previous_heroine = None
+        try:
+          next_heroine = Heroine.objects.filter(birthdate__gt=selected_heroine.birthdate).order_by('birthdate')[0]
+        except IndexError:
+          next_heroine = None
       except Heroine.DoesNotExist:
         raise Http404
   else:
     context['heroines'] = Heroine.objects.filter(is_public=True).all()
     if slug is not None:
       try:
-        context['selected_heroine'] = Heroine.objects.get(slug=slug,is_public=True)
+        selected_heroine = Heroine.objects.get(slug=slug,is_public=True)
+        try:
+          previous_heroine = Heroine.objects.filter(birthdate__lt=selected_heroine.birthdate,is_public=True).order_by('-birthdate')[0]
+        except IndexError:
+          previous_heroine = None
+        try:
+          next_heroine = Heroine.objects.filter(birthdate__gt=selected_heroine.birthdate,is_public=True).order_by('birthdate')[0]
+        except IndexError:
+          next_heroine = None
       except Heroine.DoesNotExist:
         raise Http404
+
+  context['selected_heroine'] = selected_heroine
+  context['previous_heroine'] = previous_heroine
+  context['next_heroine'] = next_heroine
 
   return TemplateResponse(request, template, context)
