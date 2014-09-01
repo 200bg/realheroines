a4138cc8a4946b8c4c9db2699593650fe636a7c9
Oliver Wilkerson <oliver.wilkerson@gmail.com>
2014-09-01 13:13:00 -0500

    added the missing 404 page;

--- rh/urls.py
+++ rh/urls.py
@@ -16,4 +16,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^404/$', 'rh.views.not_found', name='404'),
 )
+
+handler404 = 'rh.views.not_found'
